44d4f7524ecef3a0ef9652a2d5ac780514874d88
Pablo Mugica <pablo.mugica@gmail.com>
2020-11-15 15:36:12 -0300

    Multiproc add

--- nodbdb/nodb_client.py
+++ nodbdb/nodb_client.py
@@ -1,12 +1,11 @@
-import sys, csv 
-import time
+import sys, csv, time 
+from multiprocessing import Pool
 from azure.storage.blob import BlobServiceClient, ContainerClient, BlobClient, DelimitedTextDialect, BlobQueryError
 
 def get_explanations():
     return "Chillax, 'tis just a storage"
 
 def query(a_query, a_blob_url, a_file_type):
-
     if a_file_type == 'csv':
         blob_client = BlobClient.from_blob_url(blob_url= a_blob_url)
         qa_reader = blob_client.query_blob(a_query, blob_format=DelimitedTextDialect(has_header=True), encoding='utf-8')
@@ -22,6 +21,12 @@ def read_the_news():
     return "This would read the change feed. TBI (to be implemented) :D"
 
 def query_a_dir():
+    # some_args = []
+    # pool = Pool()
+    # result = pool.starmap_async(query, some_args)
+    # print("main script")
+    # print(result.get())
+    # print("end main script")
     return "TBI (to be implemented) :D"
 
 #In the future refactor to class
